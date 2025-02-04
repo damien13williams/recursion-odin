818b4a466d41b511698250aefe16e140de2c5140
damien13williams <190779546+damien13williams@users.noreply.github.com>
2025-02-04 15:16:24 -0600

    fibonacci recursive function finished

--- main.js
+++ main.js
@@ -15,11 +15,19 @@ function fibIterative(inputNum){
 
 
 // Fibonacci recursive version 
-function fibRecursive(num){
-    
+function fibRecursive(n){
+
+    // Base case
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const arr = fibRecursive(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
 }
 
 // Merge Sort
 
 
-console.log(fibIterative(8));
+console.log(fibRecursive(8));
